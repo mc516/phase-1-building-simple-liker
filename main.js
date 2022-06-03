4b8857d81a7654f9e3be253a6b0af9cecfcb7f84
Mickey <mickeyc11111@gmail.com>
2022-06-02 22:26:57 -0400

    commit tree a233f81ca756bda938e93cfb008da336d66999bb

--- main.js
+++ main.js
@@ -2,7 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let glyphs = document.querySelectorAll(".like-glyph");
+
+for (const item of glyphs){
+  item.addEventListener('click', () => {
+    mimicServerCall()
+    .then(res => {
+      console.log(res);
+      if(res === "Pretend remote server notified of action!") {
+        item.classList.add('liked');
+        item.textContent = FULL_HEART;
+      }
+    })
+    .catch(err => {
+      document.querySelector('#modal').classList.remove('hidden')
+      setTimeout(() => {
+        document.querySelector('#modal').classList.add('hidden')
+      }, 3000);
+    })
+  })
+}
+
+for (const item of glyphs) {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('liked')){
+      item.classList.remove('liked')
+      item.textContent = EMPTY_HEART;
+    } else {
+      item.classList.add('liked');
+    }
+  })
+}
+
+
+
+
+
 
 
 
